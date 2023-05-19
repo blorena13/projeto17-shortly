000ea018887f31fb569af6b3118acb13a8529bb0
Lorena <blorena1321@gmail.com>
2023-05-18 23:29:06 -0300

    implementação de cadastro e login

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,17 +1,45 @@
 import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
-export async function signup(req,res) {
-    try{
+export async function signup(req, res) {
 
-    } catch(err){
+    const { name, email, password, confirmPassword } = req.body;
 
+    try {
+        const existsEmail = await db.query(`SELECT * FROM cadastro WHERE email=$1;`, [email]);
+        if (existsEmail.rows.length !== 0) {
+            return res.status(409).send("Email já cadastrado!");
+        }
+
+        const hash = bcrypt.hashSync(password, 10);
+
+        await db.query(`INSERT INTO cadastro (name, email, password) VALUES ($1, $2, $3);`, [name, email, hash]);
+        res.sendStatus(201);
+
+    } catch (err) {
+        res.status(500).send(err.message);
     }
 }
 
-export async function signin(req, res){
-    try{
+export async function signin(req, res) {
 
-    } catch(err){
-        
+    const { email, password } = req.body;
+    try {
+        const existsUser = await db.query(`SELECT * FROM cadastro WHERE email=$1;`, [email]);
+        if (existsUser.rows.length === 0) {
+            return res.status(401).send("Email não compatível.");
+        }
+        const user = existsUser.rows[0];
+        const correctPassword = bcrypt.compareSync(password, user.password);
+        if (!correctPassword) {
+            return res.status(401).send("Senha inválida;");
+        }
+        const token = uuid();
+        await db.query(`INSERT INTO login (token) VALUES ($1);`, [token]);
+        res.status(200).send(token);
+
+    } catch (err) {
+        res.status(500).send(err.message);
     }
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { signin, signup } from "../controllers/auth.controllers.js";
+import { signUpSchema, signInSchema } from "../schemas/auth.schemas.js";
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", signup);
-authRouter.post("signin", signin);
+authRouter.post("/signup", validateSchema(signUpSchema), signup);
+authRouter.post("/signin", validateSchema(signInSchema), signin);
 
 export default authRouter;
